0ff8af1a646dec83447b3b463d250cbcca1da720
z17cx <mail@z17.cx>
2022-09-05 20:38:27 +0000

    2022-09-05 20:38:27

--- MW_EXT_Progress.i18n.magic.php
+++ MW_EXT_Progress.i18n.magic.php
@@ -6,14 +6,14 @@
  * @file
  * @ingroup Extensions
  * @licence GNU GPL
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords = [];
 
 /**
  * English.
  *
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords['en'] = [
   'progress' => [0, 'progress'],
@@ -22,7 +22,7 @@ $magicWords['en'] = [
 /**
  * Russian.
  *
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords['ru'] = [
   'progress' => [0, 'progress'],
